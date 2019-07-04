858731c02cd1614c311cf389d46d3cdeceb7ce7a
wangchao <wb-wc540140@alibaba-inc.com>
2019-07-04 15:29:48 +0800

    init

---
+++ jenkins.js
@@ -0,0 +1,21 @@
+/**
+ * 服务器部署，使用
+ * node server.js 
+ * 或者 pm2
+ * pm2 start server.js
+ * 然后使用ngxin将443或者80端口的请求代理到8080
+ */
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.send("hello world")
+  // res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9999, function() {
+  console.log('server strart on 8080')
+});

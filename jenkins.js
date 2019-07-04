07af454498d1af77c0938836db6b57dfc555d3d7
ococa <50704819+ococa@users.noreply.github.com>
2019-07-04 23:12:33 +0800

    Update jenkins.js

--- jenkins.js
+++ jenkins.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(req, res) {
-  res.send("你好 world3211")
+  res.send("你好啊jenkins world3211")
 });
 
 app.listen(9999, function() {
